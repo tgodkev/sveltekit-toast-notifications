e1dbfc8c4e44f8844df6de7107fb8b08d06e6a21
Kevin <50965376+tgodkev@users.noreply.github.com>
2023-11-21 19:21:03 -0500

    start

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+
+	// import { toast } from '$lib/toast.ts';
+	// import Toast from '$lib/Toast.svelte';
+
+
+	
+</script>
+
+<!-- {#if $toast}
+	{#each $toast as t (t.id)}
+		<Toast {t} />
+	{/each}
+{/if} -->
+
+<div class="flex flex-col min-h-screen">
+
+	<main class="transition-outer md:max-w-screen-2xl max-w-sm mx-auto">
+				<div class="flex-grow">
+					<slot />
+                </div>
+	</main>
+
+</div>
